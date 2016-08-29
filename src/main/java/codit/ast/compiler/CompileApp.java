ff67d17cd1864d7dabcdbd9c2d42aca865f29c9d
Jay Lim <iejisung@gmail.com>
2016-08-30 00:22:48 +0900

    arguments case

--- src/main/java/codit/ast/compiler/CompileApp.java
+++ src/main/java/codit/ast/compiler/CompileApp.java
@@ -10,7 +10,12 @@ public class CompileApp {
 
   public static void main(String[] args) {
 
-    Compiler compiler = new Compiler(args[0]);
+    Compiler compiler;
+    if (args[1] == null) {
+      compiler = new Compiler(args[0], args[1]);
+    } else {
+      compiler = new Compiler(args[0], args[1]);
+    }
 
     AstNode astTree = compiler.compile();
 
